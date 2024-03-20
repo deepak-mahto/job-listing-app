c84640d68b2ac1009011bc7cc44f4e43e54551c7
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-21 04:41:09 +0530

    CRUD app finalised with update and delete requests

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import EditJobPage from "./pages/EditJobPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 
 const App = () => {
@@ -32,12 +33,29 @@ const App = () => {
     });
   };
 
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route
           path="/jobs/:id"
           element={<JobPage deleteJob={deleteJob} />}
